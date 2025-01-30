789ac58c074b3b9fbb53c112f8c2051c03cd2a16
srikanth <srikanthv1709@gmail.com>
2025-01-30 16:13:22 +0530

    January 30

---
+++ src/oAuthTest/OAuthTest.java
@@ -0,0 +1,46 @@
+package oAuthTest;
+import static io.restassured.RestAssured.*;
+
+import io.restassured.path.json.JsonPath;
+public class OAuthTest {
+
+	public static void main(String[] args) {
+		String response = given()
+		.formParam("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
+		.formParam("client_secret", "erZOWM9g3UtwNRj340YYaK_W")
+		.formParam("grant_type", "client_credentials")
+		.formParam("scope", "trust")
+		.when().log().all()
+		.post("https://rahulshettyacademy.com/oauthapi/oauth2/resourceOwner/token").asString();
+		System.out.println(response);
+		JsonPath jsonPath = new JsonPath(response);
+		String accessToken = jsonPath.getString("access_token");
+		System.out.println("access token issssssssssss   "+ accessToken);
+		String getResponse = given()
+		.queryParam("access_token", accessToken)
+		.when().log().all()
+		.get("https://rahulshettyacademy.com/oauthapi/getCourseDetails").asString();
+		System.out.println(getResponse);
+		
+
+	}
+
+}
+/*
+*API Contract download
+******************************************************************
+Authorization Server EndPoint:
+https://rahulshettyacademy.com/oauthapi/oauth2/resourceOwner/token
+HTTP Method : POST
+Form parameters :
+client_id:
+692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com
+client_secret:  erZOWM9g3UtwNRj340YYaK_W
+grant_type:   client_credentials
+scope:  trust
+******************************************************************
+GetCourseDetails EndPoint (Secured by OAuth) :
+https://rahulshettyacademy.com/oauthapi/getCourseDetails
+HTTP Method : GET
+Query Parameter : access_token
+*/
