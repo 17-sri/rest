728346ee568e8292d89247d784416502b20b023a
srikanth <srikanthv1709@gmail.com>
2025-02-05 10:20:31 +0530

    05 february

---
+++ src/eCom/ECommerceAPITest.java
@@ -0,0 +1,28 @@
+package eCom;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import pojo.EComLoginRequest;
+import pojo.EComLoginResponse;
+
+import static io.restassured.RestAssured.*;
+
+public class ECommerceAPITest {
+
+	public static void main(String[] args) {// 70
+		RequestSpecification req = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com")
+				.setContentType(ContentType.JSON).build();
+		EComLoginRequest ecomLoginRequest = new EComLoginRequest();
+		ecomLoginRequest.setUserEmail("srikanthv1709@gmail.com");
+		ecomLoginRequest.setUserPassword("Selenium@123");
+		given().spec(req).body(ecomLoginRequest);
+		RequestSpecification reqLogin = given().log().all().spec(req).body(ecomLoginRequest);
+		EComLoginResponse loginResponse = reqLogin.when().post("/api/ecom/auth/login").then().log().all().extract()
+				.response().as(EComLoginResponse.class);
+		System.out.println(loginResponse.getToken());
+		System.out.println(loginResponse.getUserID());
+
+	}
+
+}
