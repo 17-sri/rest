5f58474d8e0cf692868ac6b0f2578c6f301952d5
srikanth <srikanthv1709@gmail.com>
2025-02-05 17:59:15 +0530

    februaryyyy 05

--- src/eCom/ECommerceAPITest.java
+++ src/eCom/ECommerceAPITest.java
@@ -2,11 +2,18 @@ package eCom;
 
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.specification.RequestSpecification;
 import pojo.EComLoginRequest;
 import pojo.EComLoginResponse;
+import pojo.OrderDetails;
+import pojo.Orders;
 
 import static io.restassured.RestAssured.*;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ECommerceAPITest {
 
@@ -20,9 +27,38 @@ public class ECommerceAPITest {
 		RequestSpecification reqLogin = given().log().all().spec(req).body(ecomLoginRequest);
 		EComLoginResponse loginResponse = reqLogin.when().post("/api/ecom/auth/login").then().log().all().extract()
 				.response().as(EComLoginResponse.class);
-		System.out.println("THE TOKEN IS   :   "+ loginResponse.getToken());
-		System.out.println("THE USERID IS   :   "+ loginResponse.getUserId());
+		String token = loginResponse.getToken();
+		String userId = loginResponse.getUserId();
+		System.out.println("THE TOKEN IS   :   " + token);
+		System.out.println("THE USERID IS   :   " + loginResponse.getUserId());
 
+		RequestSpecification addProductBaseReq = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com")
+				.addHeader("authorization", token).build(); // 71
+		RequestSpecification reqAddProduct = given().log().all().spec(addProductBaseReq).param("productName", "Laptop")
+				.param("productAddedBy", userId).param("productCategory", "Electronics")
+				.param("productSubCategory", "Electronics").param("productPrice", "7777")
+				.param("productDescription", "Dell").param("productFor", "personal use")
+				.multiPart("productImage", new File("C://Rest_Course//screen.png"));
+
+		String addProductResponse = reqAddProduct.when().post("/api/ecom/product/add-product").then().log().all()
+				.extract().asString();
+		JsonPath jsonPath = new JsonPath(addProductResponse);
+		String productId = jsonPath.get("productId");
+		System.out.println("productId IS  :  " + productId);
+
+		RequestSpecification createorderBaseReq = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com")
+				.addHeader("authorization", token).setContentType(ContentType.JSON).build();// 72
+		OrderDetails orderDetails = new OrderDetails();
+		orderDetails.setCountry("India");
+		orderDetails.setProductOrderedId(productId);
+		List<OrderDetails> orderDetailList = new ArrayList<OrderDetails>();
+		orderDetailList.add(orderDetails);
+		Orders orders = new Orders();
+		orders.setOrders(orderDetailList);
+		RequestSpecification createOrderReq = given().log().all().spec(createorderBaseReq).body(orders);
+		String responseAddOrder = createOrderReq.when().post("/api/ecom/order/create-order").then().log().all()
+				.extract().response().asString();
+		System.out.println("product ordered Id is   " + responseAddOrder);
 	}
 
 }
--- src/pojo/EComLoginResponse.java
+++ src/pojo/EComLoginResponse.java
@@ -4,28 +4,29 @@ public class EComLoginResponse {
 	String token;
 	String userId;
 	String message;
-	
+
 	public String getToken() {
 		return token;
 	}
+
 	public void setToken(String token) {
 		this.token = token;
 	}
+
 	public String getUserId() {
 		return userId;
 	}
+
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+
 	public String getMessage() {
 		return message;
 	}
+
 	public void setMessage(String message) {
 		this.message = message;
 	}
-	
-	
-	
-	
 
 }
